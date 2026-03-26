98c11c4c435b5e23d9a41e2e09719af55b5c7f0f
JAYANTI THAKOR <jyantithakor941@gmail.com>
2026-03-26 17:15:43 +0530

    make greet class

---
+++ 06_Class.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Hello{
+    public:
+        void greet(){
+            cout<<"Hey Developers.!!";
+        }
+};
+int main(){
+    Hello h;
+    h.greet();
+    return 0;
+}
